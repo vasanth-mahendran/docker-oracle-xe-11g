e117ac851a55407c57d7f7d1f0a603eed98186aa
vasanth mahendran <vasanth.mahendran@objectedge.com>
2017-10-09 17:08:13 -0700

    Update startup.sh

--- assets/startup.sh
+++ assets/startup.sh
@@ -15,11 +15,11 @@ export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export PATH=$ORACLE_HOME/bin:$PATH
 export ORACLE_SID=XE
 
-if ! [ "$ORACLE_PASSWORD_VERIFY" = true ]; then
-  echo "ALTER PROFILE DEFAULT LIMIT PASSWORD_VERIFY_FUNCTION NULL;" | sqlplus -s sys/system as sysdba
-  echo "alter profile DEFAULT limit password_life_time UNLIMITED;" | sqlplus -s sys/system as sysdba
-  echo "alter user SYSTEM identified by oracle account unlock;" | sqlplus -s sys/system as sysdba
-  echo "alter system set sec_case_sensitive_logon=false;" | sqlplus -s sys/system as sysdba
+#if ! [ "$ORACLE_PASSWORD_VERIFY" = true ]; then
+#  echo "ALTER PROFILE DEFAULT LIMIT PASSWORD_VERIFY_FUNCTION NULL;" | sqlplus -s sys/system as sysdba
+#  echo "alter profile DEFAULT limit password_life_time UNLIMITED;" | sqlplus -s sys/system as sysdba
+#  echo "alter user SYSTEM identified by oracle account unlock;" | sqlplus -s sys/system as sysdba
+#  echo "alter system set sec_case_sensitive_logon=false;" | sqlplus -s sys/system as sysdba
 fi
 
 if [ "$ORACLE_ENABLE_XDB" = true ]; then
